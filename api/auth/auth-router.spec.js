69c1c1674ce01020fe263d680fbd2c2dd2fd3bc6
TheHaunted <kevin_lopez@live.com>
2019-09-24 18:02:39 -0500

    tries to configure test environment

---
+++ api/auth/auth-router.spec.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const server = require('../server.js');
+
+describe('/api/auth', () => {
+    describe('/register register new user', () => {
+        it('should return 201', () => {
+            return request(server).post('api/auth/register')
+                .send({
+                    username: 'test',
+                    password: 'pass'
+                })
+                .then(res => expect(res.status).toBe(200))
+        })
+    })
+})
