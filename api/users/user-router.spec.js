ae96721f9253d6088459c796fbf3cc095c84012b
TheHaunted <kevin_lopez@live.com>
2019-09-26 17:38:42 -0500

    changes cors exception from static to dynamic

--- api/users/user-router.spec.js
+++ api/users/user-router.spec.js
@@ -4,7 +4,8 @@ const server = require('../server.js');
 describe('/api/users', () => {
     describe('/:id', () => {
         it('should return user', () => {
-            
+            return request(server).get('/api/users/1')
+                .then(res => expect(res.user.id).toBe('1'))
         })
     })
 })
