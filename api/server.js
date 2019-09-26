3eb389c7630931cf99612914ce17620c23104330
TheHaunted <kevin_lopez@live.com>
2019-09-26 16:07:27 -0500

    adds cors exception for zeit deployment

--- api/server.js
+++ api/server.js
@@ -15,14 +15,7 @@ server.use(helmet());
 const whitelist = ['http://localhost:3000', 'https://secretfamilyrecipesfe.tsbiswell.now.sh'];
 const corsOptions = {
     credentials: true,
-    origin: function(origin, callback){
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-          const msg = 'The CORS policy for this site does not ' +
-                    'allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
+    origin: whitelist[1]
     }
 };
 server.use(cors(corsOptions));
