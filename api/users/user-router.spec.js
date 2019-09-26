ceb48bedbf445e13e5bed360fe9585f3cbb65ff8
TheHaunted <kevin_lopez@live.com>
2019-09-26 15:04:37 -0500

    adds cors exception for zeit deployment

---
+++ api/users/user-router.spec.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const server = require('../server.js');
+
+describe('/api/users', () => {
+    describe('/:id', () => {
+        it('should return user', () => {
+            
+        })
+    })
+})
